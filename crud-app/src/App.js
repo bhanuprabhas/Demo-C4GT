289f981b19c868032d89c872b51cecdb0b8e694f
Ajaysigireddy <ajaysigiredy908@gmail.com>
2025-08-26 16:03:19 +0530

    Ajay addded the line in App.js2

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with react test tets 
+          Learn React with react test tetstst 
         </a>
       </header>
     </div>
